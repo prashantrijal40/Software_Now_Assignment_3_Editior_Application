6c1b402fff19271b6988d9b2a48e447eb5e43164
Asmita Shrestha <user.name>
2026-02-02 20:11:05 +0930

    update: make the main file more clear and understable

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+#import the ImageEditorApp class that contains all GUI features of the editor
 from gui_app import ImageEditorApp
+
+#import tkinter library to create the window and handle GUI
 import tkinter as tk
 
+# start the application only when this file is run directly
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = ImageEditorApp(root)
-    root.mainloop()
+    root = tk.Tk()                #create main window
+    app = ImageEditorApp(root)    #load the image editor into the window
+    root.mainloop()               # run the GUI Loop
