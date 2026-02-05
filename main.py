2a5ed483da5efa88aec3e3e6229750c9a349d4ca
minhhoangbuicdu <minhhoangbuicdu@gmail.com>
2026-02-05 22:13:45 +0930

    update main

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ Attributes used in this module:
     ImageEditorApp: The main GUI application class from gui_app module.
     tk: tkinter library for creating the GUI window.
 """
+
 # Import the ImageEditorApp class from gui_app.py
 # Import the Tkinter library for creating GUI applications
 from gui_app import ImageEditorApp
@@ -19,9 +20,7 @@ if __name__ == "__main__":
     """Initialize and run the Image Editor application."""
     # To create the main tkinter window that will host the application
     root = tk.Tk()
-    
     # To instantiate the ImageEditorApp class and pass the root window to it
     app = ImageEditorApp(root)
-    
     # To start the event loop to display the GUI and handle user interactions
     root.mainloop()
